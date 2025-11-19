a795fed92e72d31abc674d2fd545b9a2fca056c6
Sikander Riaz <sikanderriazkhan@gmail.com>
2025-11-19 13:38:07 +0500

    fix:s3-tf-state-dev-2026 in backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-tf-state-dev"
+    bucket         = "s3-tf-state-dev-2026"
     key            = "dev/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-locks-dev"
